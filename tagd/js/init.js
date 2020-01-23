62b262d5bd9873faa1bb844af3754e41863b15e2
sstirling <stephenstirling@gmail.com>
2020-01-23 14:17:54 -0500

    second chart test

--- tagd/js/init.js
+++ tagd/js/init.js
@@ -87,6 +87,37 @@ $(window).load(function() {
         var result = [];
         var count = 1;
 
+        google.charts.load('current', {packages: ['corechart', 'bar']});
+        google.charts.setOnLoadCallback(drawBasic);
+
+function drawBasic() {
+
+      var data = google.visualization.arrayToDataTable([
+        ['', '',],
+        ['New York City, NY', 8175000],
+        ['Los Angeles, CA', 3792000],
+        ['Chicago, IL', 2695000],
+        ['Houston, TX', 2099000],
+        ['Philadelphia, PA', 1526000]
+      ]);
+
+      var options = {
+        /* title: 'Population of Largest U.S. Cities',
+         */chartArea: {width: '50%'},
+        hAxis: {
+/*           title: 'Total Population',
+           */          minValue: 0
+        },
+        vAxis: {
+          title: 'City'
+        }
+      };
+
+      var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+
+      chart.draw(data, options);
+    }
+
 
         $.each(tabletop.sheets(sheetname).all(), function(i, v) {
 
@@ -199,36 +230,7 @@ $(window).load(function() {
 
             });
         });
-        google.charts.load('current', {packages: ['corechart', 'bar']});
-        google.charts.setOnLoadCallback(drawBasic);
 
-function drawBasic() {
-
-      var data = google.visualization.arrayToDataTable([
-        ['', '',],
-        ['New York City, NY', 8175000],
-        ['Los Angeles, CA', 3792000],
-        ['Chicago, IL', 2695000],
-        ['Houston, TX', 2099000],
-        ['Philadelphia, PA', 1526000]
-      ]);
-
-      var options = {
-        /* title: 'Population of Largest U.S. Cities',
-         */chartArea: {width: '50%'},
-        hAxis: {
-/*           title: 'Total Population',
-           */          minValue: 0
-        },
-        vAxis: {
-          title: 'City'
-        }
-      };
-
-      var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-
-      chart.draw(data, options);
-    }
 
     };
 
